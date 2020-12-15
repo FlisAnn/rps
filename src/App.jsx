1f68a57019113e63268c3ba41ec49d88c269d4e6
flis_ann <ann.flismark@gmail.com>
2020-12-15 21:32:41 +0100

    Adding scissors button

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ function App() {
     <>
      <button data-cy="rock-button">Rock</button>
      <button data-cy="paper-button">Paper</button>
+     <button data-cy="scissors-button">Scissors</button>
     </>
   );
 }
