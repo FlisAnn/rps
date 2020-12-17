b3af6e7dd310cf3edad3ae4fa44f13ae5ab690a8
flis_ann <ann.flismark@gmail.com>
2020-12-17 21:53:42 +0100

    Adding last game logic, working on App page to display results

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,49 @@
-import React from "react";
+import React, { Component } from "react";
+import winner from "./modules/ChoosingAWinner";
 import ChoosingAWinner from "./modules/ChoosingAWinner";
 
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.weapons = ["rock", "paper", "scissors"];
+    this.state = {
+      humanChoice: "",
+    };
+  }
 
-function App() {
-  return (
-    <>
-      <button data-cy="rock-button">Rock</button>
-      <button data-cy="paper-button">Paper</button>
-      <button data-cy="scissors-button">Scissors</button>
+  startGame = async (e) => {
+    const game = Math.floor(Math.random() * 3);
+    const result = await winner(e.target.name, this.weapons[game]);
+    this.setState({ result: result });
+  };
 
-      <button data-cy="rock-computer">Rock</button>
-      <button data-cy="paper-computer">Paper</button>
-      <button data-cy="scissors-computer">Scissors</button>
-      <p data-cy="tie-message">It's a tie!</p>
-      <p data-cy="human-wins">You win!</p>
-      <p data-cy="computer-wins">Computer wins!</p>
-    </>
-  );
+  render() {
+    let result;
+
+    if (this.state.result) {
+      result = <p>{this.state.result}</p>;
+    }
+
+    return (
+      <>
+        <button data-cy="rock-button" onClick={this.startGame}>
+          Rock
+        </button>
+        <button data-cy="paper-button" onClick={this.startGame}>
+          Paper
+        </button>
+        <button data-cy="scissors-button" onClick={this.startGame}>
+          Scissors
+        </button>
+
+        {/* <button data-cy="rock-computer">Rock</button>
+        <button data-cy="paper-computer">Paper</button>
+        <button data-cy="scissors-computer">Scissors</button> */}
+        <p data-cy="tie-message">It's a tie!</p>
+        <p data-cy="human-wins">You win!</p>
+        <p data-cy="computer-wins">Computer wins!</p>
+      </>
+    );
+  }
 }
-
 export default App;
--- src/modules/ChoosingAWinner.js
+++ src/modules/ChoosingAWinner.js
@@ -9,10 +9,21 @@ const winner = (humanChoice, computerChoice) => {
       return (result = "Computer selected ${computerChoice} and you won!");
     case humanChoice == "rock" && computerChoice == "paper":
       return (result = "Computer selected ${computerChoice} and Computer won!");
-      break;
 
-    default:
-      break;
+    case humanChoice == "paper" && computerChoice == "paper":
+      return (result = "Computer selected ${computerChoice} and it is a tie");
+    case humanChoice == "paper" && computerChoice == "rock":
+      return (result = "Computer selected ${computerChoice} and you won!");
+    case humanChoice == "paper" && computerChoice == "scissors":
+      return (result = "Computer selected ${computerChoice} and Computer won!");
+
+    case humanChoice == "scissors" && computerChoice == "scissors":
+      return (result = "Computer selected ${computerChoice} and it is a tie");
+    case humanChoice == "scissors" && computerChoice == "paper":
+      return (result = "Computer selected ${computerChoice} and you won!");
+    case humanChoice == "scissors" && computerChoice == "rock":
+      return (result = "Computer selected ${computerChoice} and Computer won!");
   }
+  return result;
 };
 export default winner;
