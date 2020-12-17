a041441d6279aadc056b84e922970cde39c492df
flis_ann <ann.flismark@gmail.com>
2020-12-17 21:22:44 +0100

    creating gameplay logic

--- src/modules/ChoosingAWinner.js
+++ src/modules/ChoosingAWinner.js
@@ -1,17 +1,18 @@
 const winner = (humanChoice, computerChoice) => {
+  let result;
+  let history;
 
-  let result
-  let history
+  switch (true) {
+    case humanChoice == "rock" && computerChoice == "rock":
+      return (result = "Computer selected ${computerChoice} and it is a tie");
+    case humanChoice == "rock" && computerChoice == "scissors":
+      return (result = "Computer selected ${computerChoice} and you won!");
+    case humanChoice == "rock" && computerChoice == "paper":
+      return (result = "Computer selected ${computerChoice} and Computer won!");
+      break;
 
-switch (true) {
-  case humanChoice == "rock" && computerChoice == "rock":
-    return result
-    
-    break;
-
-  default:
-    break;
-}
-
-
-}
+    default:
+      break;
+  }
+};
+export default winner;
