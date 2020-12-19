b677a668f16b1c0d723c2476877f4890da7fe2fa
flis_ann <ann.flismark@gmail.com>
2020-12-19 15:59:16 +0100

    Styling of buttons is now nice

--- src/App.jsx
+++ src/App.jsx
@@ -40,122 +40,78 @@ class App extends Component {
       <Container id="container">
         <TopHeader />
         <DisplayRules />
-        <Button data-cy="rock-button" name="rock" onClick={this.startGame}>
+
+        {/* <Button
+          data-cy="rock-button"
+          name="rock"
+          onClick={this.startGame}
+          color="pink"
+        >
           Rock
         </Button>
-        <Button data-cy="paper-button" name="paper" onClick={this.startGame}>
+        <Button
+          data-cy="paper-button"
+          name="paper"
+          onClick={this.startGame}
+          color="yellow"
+        >
           Paper
         </Button>
-        <Button data-cy="scissors-button" name="scissors" onClick={this.startGame}>
+        <Button
+          data-cy="scissors-button"
+          name="scissors"
+          onClick={this.startGame}
+          color="teal"
+        >
           Scissors
-        </Button>
-        <div id = "result">{result}</div>
-        {/* <div>
-          <button
-            name="Rock"
-            data-cy="rock-button"
-            link onClick={this.startGame}
-            
-            name="hand rock outline"
-            size="huge"
-            color="pink"
-          />
-          </div>
-          <div>
-           <Icon
-            name="paper"
-            data-cy="paper-button"
-            link onClick={this.startGame}
-            name="hand paper outline"
-            size="huge"
-            color="pink"
-          />
-          </div>
-          <div>
-           <Icon
-            name="scissors"
-            data-cy="scissors-button"
-            link onClick={this.startGame}
+        </Button> */}
         
-            name="hand scissors outline"
-            size="huge"
-            color="pink"
-          />
-        </div> */}
-       
-        {/* <Segment placeholder color="teal">
-          <Grid columns={2} stackable textAlign="center">
-            <Divider vertical>VS</Divider>
 
+        <Segment placeholder color="teal">
+          <Grid columns={2} stackable textAlign="center">
             <Grid.Row verticalAlign="middle">
               <Grid.Column>
                 <Header as="h2" color="grey">
-                  You
+                  Pick your weapon
                 </Header>
                 <Grid.Row icon>
                   <Button.Group>
-                    <Button icon data-cy="rock-button">
-                      <Icon
-                        name="Rock"
-                        data-cy="rock-button"
-                        onClick={this.startGame}
-                        name="hand rock outline"
-                        size="huge"
-                        color="pink"
-                      />
+                    <Button
+                      data-cy="rock-button"
+                      name="rock"
+                      onClick={this.startGame}
+                      color="pink"
+                    >
+                      Rock
                     </Button>
                     <Button.Or />
                     <Button
-                      icon
-                      name="paper"
                       data-cy="paper-button"
+                      name="paper"
                       onClick={this.startGame}
+                      color="yellow"
                     >
-                      <Icon
-                        name="hand paper outline"
-                        size="huge"
-                        color="yellow"
-                        id="humanPaper"
-                      />
+                      Paper
                     </Button>
                     <Button.Or />
                     <Button
-                      icon
                       data-cy="scissors-button"
+                      name="scissors"
                       onClick={this.startGame}
+                      color="teal"
                     >
-                      <Icon
-                        name="hand scissors outline"
-                        size="huge"
-                        color="teal"
-                        id="humanScissors"
-                      />
+                      Scissors
                     </Button>
                   </Button.Group>
                 </Grid.Row>
-
-                <Header id="human" color="grey">
-                  Rock, Paper or Scissors?
-                </Header>
               </Grid.Column>
             </Grid.Row>
           </Grid>
-        </Segment> */}
+        </Segment>
+        <div id="result">{result}</div>
+       
 
-        {/* <button data-cy="rock-button" name="rock" onClick={this.startGame}>
-          Rock
-        </button>
-        <button data-cy="paper-button" name="paper" onClick={this.startGame}>
-          Paper
-        </button>
-        <button data-cy="scissors-button" name="scissors" onClick={this.startGame}>
-          Scissors
-        </button> */}
-
-        
-        {/* <button data-cy="rock-computer">Rock</button>
-        <button data-cy="paper-computer">Paper</button>
-        <button data-cy="scissors-computer">Scissors</button> */} 
+    
         {/* <p data-cy="tie-message">It's a tie!</p>
         <p data-cy="human-wins">You win!</p>
         <p data-cy="computer-wins">Computer wins!</p> */}
