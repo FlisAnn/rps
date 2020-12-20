3cc527c8c7ef96bc7e313315fea735b177243c3b
flis_ann <ann.flismark@gmail.com>
2020-12-20 20:29:37 +0100

    Making the text in the footer be in the middle

--- src/modules/ChoosingAWinner.js
+++ src/modules/ChoosingAWinner.js
@@ -2,25 +2,25 @@ const winner = (humanChoice, computerChoice) => {
   let result;
 
   switch (true) {
-    case humanChoice == "rock" && computerChoice == "rock":
+    case humanChoice === "rock" && computerChoice === "rock":
       return (result = `It's a Tie! Computer also picked ${computerChoice}`);
-    case humanChoice == "rock" && computerChoice == "scissors":
+    case humanChoice === "rock" && computerChoice === "scissors":
       return (result = `You win! Computer picked ${computerChoice}`);
-    case humanChoice == "rock" && computerChoice == "paper":
+    case humanChoice === "rock" && computerChoice === "paper":
       return (result = `Computer wins! Computer picked ${computerChoice}`);
 
-    case humanChoice == "paper" && computerChoice == "paper":
+    case humanChoice === "paper" && computerChoice === "paper":
       return (result = `It's a Tie! Computer also picked ${computerChoice}`);
-    case humanChoice == "paper" && computerChoice == "rock":
+    case humanChoice === "paper" && computerChoice === "rock":
       return (result = `You win! Computer picked ${computerChoice}`);
-    case humanChoice == "paper" && computerChoice == "scissors":
+    case humanChoice === "paper" && computerChoice === "scissors":
       return (result = `Computer wins! Computer picked ${computerChoice}`);
 
-    case humanChoice == "scissors" && computerChoice == "scissors":
+    case humanChoice === "scissors" && computerChoice === "scissors":
       return (result = `It's a Tie! Computer also picked ${computerChoice}`);
-    case humanChoice == "scissors" && computerChoice == "paper":
+    case humanChoice === "scissors" && computerChoice === "paper":
       return (result = `You win! Computer picked ${computerChoice}`);
-    case humanChoice == "scissors" && computerChoice == "rock":
+    case humanChoice === "scissors" && computerChoice === "rock":
       return (result = `Computer wins! Computer picked ${computerChoice}`);
   }
   return result;
