7ed83e4650e70ff05239b40060359431b2971a0d
flis_ann <ann.flismark@gmail.com>
2020-12-20 20:16:58 +0100

    Removing comented out code

--- cypress/integration/userCanSeePlayButtons.feature.js
+++ cypress/integration/userCanSeePlayButtons.feature.js
@@ -9,16 +9,8 @@ describe("A User can see play buttons", () => {
   it("Player can choose Paper", () => {
     cy.get("[data-cy='paper-button']").click();
   });
-  
+
   it("Player can choose Scissors", () => {
     cy.get("[data-cy='scissors-button']").click();
   });
-
-  // it('shows the computer options', () => {
-  //   cy.get("[data-cy='rock-computer']").click();
-  //   cy.get("[data-cy='paper-computer']").click();
-  //   cy.get("[data-cy='scissors-computer']").click();
-    
-  // });
-
 });
